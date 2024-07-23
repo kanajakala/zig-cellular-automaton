31e4b338efe4a3d1c937d6854a66577c58b94c24
kanajakala <xiron.kanajakala@gmail.com>
2024-07-23 23:56:24 +0200

    fixed some bugs and made some graphical changes

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a very simple project to learn more about zig
 
+![cool image of the cellular automaton in action](images/screen.png)
+
 
 ## Building
 If you wish to run the app simply execute
--- src/main.zig
+++ src/main.zig
@@ -5,18 +5,18 @@ const capy = @import("capy");
 pub usingnamespace capy.cross_platform;
 
 //random
-var prng = std.rand.DefaultPrng.init(42);
+var prng = std.rand.DefaultPrng.init(43);
 const rand = prng.random();
 const stdout = std.io.getStdOut().writer();
 
 //timer
 
 //consts
-const size_x: u20 = 200;
-const size_y: u20 = 200;
-const cell_size: u8 = 4;
+const size_x: u20 = 600;
+const size_y: u20 = 600;
+const size: u8 = 2;
 //used when drawing with ellipses
-//const cell_size_float: f32 = cell_size;
+const size_float: f32 = size;
 
 //initializing the Game 2D array and the temp array wich is used to iterate the automaton
 var Game: [size_y][size_x]u2 = undefined;
@@ -34,7 +34,7 @@ fn populateGame(game: *[size_y][size_x]u2) void {
 }
 
 //this function is used to return the ammount of values n in a neighborhood
-fn count_n(n: *const [8]u2, number: u2) u8 {
+fn count_n(comptime n_of_neighbors: u8, n: [n_of_neighbors]u2, number: u2) u8 {
     var out: u8 = 0;
     for (n) |i| {
         if (i == number) {
@@ -42,6 +42,19 @@ fn count_n(n: *const [8]u2, number: u2) u8 {
         }
     }
     return out;
+}
+
+fn count_majority(n: [8]u2) u2 {
+    const a = count_n(8, n, 0);
+    const b = count_n(8, n, 1);
+    const c = count_n(8, n, 2);
+    if (a >= b and a >= c) {
+        return 0;
+    } else if (b >= a and b >= c) {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 
 const iErr = error{InvalidValue};
@@ -57,13 +70,13 @@ fn iter() !void {
 
             //Updating the automaton
             switch (temp[y][x]) {
-                0 => if (count_n(&n, 1) >= 3) {
+                0 => if (count_n(8, n, 1) >= 3) {
                     Game[y][x] = 1;
                 },
-                1 => if (count_n(&n, 2) >= 3) {
+                1 => if (count_n(8, n, 2) >= 3) {
                     Game[y][x] = 2;
                 },
-                2 => if (count_n(&n, 0) >= 3) {
+                2 => if (count_n(8, n, 0) >= 3) {
                     Game[y][x] = 0;
                 },
                 else => {
@@ -76,30 +89,42 @@ fn iter() !void {
     //std.time.sleep(1000 * std.time.ns_per_ms);
 }
 
-fn triangle(ctx: *capy.DrawContext, x: i32, y: i32) void {
-    ctx.line(x, y, x + cell_size, y);
-    ctx.line(x + cell_size, y, x + cell_size, y + cell_size);
-    ctx.line(x + cell_size, y + cell_size, x, y);
+fn triangle(ctx: *capy.DrawContext, x: i32, y: i32, t_size: i16) void {
+    ctx.line(x, y, x + t_size, y);
+    ctx.line(x + size, y, x + t_size, y + size);
+    ctx.line(x + size, y + t_size, x, y);
     ctx.fill();
 }
 
 fn onDraw(c: *capy.Canvas, ctx: *capy.DrawContext) !void {
     _ = c;
 
-    for (Game, 0..) |row, y| {
-        for (row, 0..) |cell, x| {
-            switch (cell) {
-                0 => ctx.setColor(0, 0.459, 0.643),
+    for (1..(size_y - 1)) |y| {
+        for (1..(size_x - 1)) |x| {
+            switch (Game[y][x]) {
+                0 => {
+                    const n_n: [4]u2 = .{ Game[y - 1][x], Game[y + 1][x], Game[y][x - 1], Game[y][x + 1] };
+                    if (count_n(4, n_n, 0) != 4 and n_n[1] == 0 and n_n[3] == 0) {
+                        ctx.setColor(0.9, 0.9, 0.9);
+                    } else if (count_n(4, n_n, 0) != 4 and n_n[2] == 0 and n_n[0] == 0) {
+                        ctx.setColor(0.08, 0.08, 0.08);
+                    } else {
+                        const yf: f32 = @floatFromInt(y);
+                        ctx.setColor(0 + (yf / size_y), 0.459, 0.643);
+                    }
+                    ctx.rectangle(@intCast(x * size), @intCast(y * size), size, size);
+                    ctx.fill();
+                },
                 1 => ctx.setColor(0.294, 0.267, 0.325),
                 2 => ctx.setColor(1, 0.502, 0.4),
                 else => {
                     return iErr.InvalidValue;
                 },
             }
-            //triangle(ctx, @intCast(x * cell_size), @intCast(y * cell_size));
-            ctx.rectangle(@intCast(x * cell_size), @intCast(y * cell_size), cell_size, cell_size);
-            //ctx.ellipse(@intCast(x * cell_size), @intCast(y * cell_size), cell_size_float * 2, cell_size_float * 2);
-            ctx.fill();
+            //ctx.ellipse(@intCast(x * size), @intCast(y * size), size_float, size_float);
+            //triangle(ctx, @intCast(x * size), @intCast(y * size));
+            //ctx.rectangle(@intCast(x * size), @intCast(y * size), size, size);
+            //ctx.fill();
         }
     }
     sync_fix = true;
@@ -107,12 +132,13 @@ fn onDraw(c: *capy.Canvas, ctx: *capy.DrawContext) !void {
 }
 
 fn draw(c: *capy.Canvas) !void {
-    var timer = try std.time.Timer.start();
+    const timer = try std.time.Timer.start();
+    _ = timer;
     while (true) {
         try c.requestDraw();
         if (sync_fix) {
             try iter();
-            std.debug.print("time: {}ms   \r", .{timer.lap() / 1_000_000});
+            //std.debug.print("time: {}ms   \r", .{timer.lap() / 1_000_000});
         }
     }
 }
@@ -127,12 +153,12 @@ pub fn main() !void {
 
     //creating the canvas
     const canvas = capy.canvas(.{
-        .preferredSize = capy.Size.init(size_x * cell_size, size_y * cell_size),
+        .preferredSize = capy.Size.init(size_x * size, size_y * size),
         .ondraw = @as(*const fn (*anyopaque, *capy.DrawContext) anyerror!void, @ptrCast(&onDraw)),
     });
 
-    window.setTitle("Cellular automaton test");
-    window.setPreferredSize(size_x * cell_size, size_y * cell_size);
+    window.setTitle("Cellular Automaton: RPS");
+    window.setPreferredSize(size_x * size, size_y * size);
 
     try window.set(canvas);
     window.show();
